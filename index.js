2cf0eb7c4b30ec0f97b6425a4ef705a4e6e672c6
Alex <102384477+nedelcualexioan@users.noreply.github.com>
2023-12-06 22:51:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var buttons = document.getElementsByTagName("button");
+
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click", function (){
+        
+       
+        
+
+    })
+}
+
+
+
+
+
+
+
+/*var audio = new Audio("sounds/tom-1.mp3");
+audio.play();*/
